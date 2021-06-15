beea05ecd638b956598ad56ca0ff07a3528d9ed0
Richa <richnag06@gmail.com>
2021-06-15 11:18:11 -0700

    Initial Commit

---
+++ RichaGitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichaGitHubPractice
+//
+//  Created by Apple on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
